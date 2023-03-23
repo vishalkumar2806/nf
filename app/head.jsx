35fee7137ce442249af63d5ac973fba545085d0d
unknown <vishalkumar2806@gmail.com>
2023-03-23 13:20:52 -0500

    fix head

--- app/head.jsx
+++ app/head.jsx
@@ -11,15 +11,7 @@ export default function Head() {
       data-expires="2018-11-26"
       content="Aob+++752GiUzm1RNSIkM9TINnQDxTlxz02v8hFJK/uGO2hmXnJqH8c/ZpI05b2nLsHDhGO3Ce2zXJUFQmO7jA4AAAB1eyJvcmlnaW4iOiJodHRwczovL25ldGZsaXguY29tOjQ0MyIsImZlYXR1cmUiOiJFbmNyeXB0ZWRNZWRpYUhkY3BQb2xpY3lDaGVjayIsImV4cGlyeSI6MTU0MzI0MzQyNCwiaXNTdWJkb21haW4iOnRydWV9"
     />
-    <title>Netflix</title>
-    <meta
-      content="watch movies, movies online, watch TV, TV online, TV shows online, watch TV shows, stream movies, stream tv, instant streaming, watch online, movies, watch movies India, watch TV online, no download, full length movies"
-      name="keywords"
-    />
-    <meta
-      content="Watch Netflix movies &amp; TV shows online or stream right to your smart TV, game console, PC, Mac, mobile, tablet and more."
-      name="description"
-    />
+    <title>Net flix</title>
     <meta
       name="viewport"
       content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0"
@@ -32,25 +24,6 @@ export default function Head() {
       rel="apple-touch-icon"
       href="https://assets.nflxext.com/us/ffe/siteui/common/icons/nficon2016.png"
     />
-    <script></script>
-    <meta
-      property="og:description"
-      content="Watch Netflix movies &amp; TV shows online or stream right to your smart TV, game console, PC, Mac, mobile, tablet and more."
-    />
-    <meta
-      property="al:ios:url"
-      content="nflx://www.netflix.com/signup/registration?locale=en-IN"
-    />
-    <meta property="al:ios:app_store_id" content="363590051" />
-    <meta property="al:ios:app_name" content="Netflix" />
-    <meta
-      property="al:android:url"
-      content="nflx://www.netflix.com/signup/registration?locale=en-IN"
-    />
-    <meta property="al:android:package" content="com.netflix.mediaclient" />
-    <meta property="al:android:app_name" content="Netflix" />
-    <meta name="twitter:card" content="player" />
-    <meta name="twitter:site" content="@netflix" />
     <link
       type="text/css"
       rel="stylesheet"
@@ -67,8 +40,6 @@ export default function Head() {
     <script src="/assets/js/nof.js" async="" type="text/javascript"></script>
     <script src="/assets/js/script.js" async="" type="text/javascript"></script>
     <style type="text/css" id="onetrust-style"></style>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.3/jquery.min.js" async="" type="text/javascript" integrity="sha512-STof4xm1wgkfm7heWqFJVn58Hm3EtS31XFaagaa8VMReCXAkQnJZ+jEy8PCC/iT18dFy95WcExNHFTqLyp72eQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>  
-    <script src="/assets/js/jquery.inputmask.min.js" async="" type="text/javascript" ></script>
     </>
   )
 }
